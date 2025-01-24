34a31353bd9db46b7d534eebdd3f4b4ab204dd0a
niaka3dayo <65211260+niaka3dayo@users.noreply.github.com>
2025-01-24 18:29:24 +0900

    feat: update Next.js configuration and package scripts for improved build process

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from 'next';
 
+/**
+ * Define the Next.js configuration.
+ */
 const nextConfig: NextConfig = {
     output: 'export',
-    distDir: 'docs',
     images: {
         unoptimized: true,
     },
